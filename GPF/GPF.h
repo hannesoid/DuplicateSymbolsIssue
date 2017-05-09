61468022ba2ce2690ea1553be896bcc29aa0d562
Hannes Oud <hannes.oud.dev@gmail.com>
2017-05-09 09:44:59 +0200

    Add framework target and podfile

---
+++ GPF/GPF.h
@@ -0,0 +1,19 @@
+//
+//  GPF.h
+//  GPF
+//
+//  Created by Hannes Oud on 09.05.17.
+//  Copyright © 2017 Hannesoid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPF.
+FOUNDATION_EXPORT double GPFVersionNumber;
+
+//! Project version string for GPF.
+FOUNDATION_EXPORT const unsigned char GPFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPF/PublicHeader.h>
+
+
